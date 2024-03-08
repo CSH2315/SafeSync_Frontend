d1631c3e77f763dae05aaff2ba426ad0bdb65ecd
CSH2315 <choish00123@gmail.com>
2024-03-08 20:22:23 +0900

    로그인 UI  구현

---
+++ safesync_frontend/lib/login.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(LoginApp());
+}
+
+// 로그인 페이지
+class LoginApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Demo',
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // 아이디, 비밀번호 정보
+  TextEditingController _phoneNumberController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  // keep logged in 여부
+  bool _keepLoggedIn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // 휴대전화 번호 입력 텍스트필드
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 300,
+                  child: CupertinoTextField(
+                    controller: _phoneNumberController,
+                    placeholder: 'Phone Number',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              // 비밀번호 입력 텍스트필드
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 300,
+                  child: CupertinoTextField(
+                    controller: _passwordController,
+                    placeholder: 'Password',
+                    textAlign: TextAlign.center,
+                    obscureText: true,
+                  ),
+                ),
+              ),
+              // Checkbox asking whether to 'Keep logged in'
+              SizedBox(
+                width: 300,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Keep logged in ',
+                    ),
+                    Checkbox(
+                      value: _keepLoggedIn,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          _keepLoggedIn = value ?? false;
+                        });
+                      },
+                    ),
+                    Text('    '),
+                  ],
+                ),
+              ),
+              // 로그인 버튼
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 250,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Continue'),
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.red), // 버튼의 배경색을 빨간색으로 변경
+                      foregroundColor: MaterialStateProperty.all<Color>(Colors.white), // 버튼의 글자색을 하얀색으로 변경
+                    ),
+                  ),
+                ),
+              ),
+              // 회원가입 텍스트
+              GestureDetector(
+                onTap: () {
+                  // TODO: 회원가입 페이지로 이동하는 로직 추가
+                },
+                child: Text(
+                  "You don't have an account? Sign in",
+                  style: TextStyle(color: Colors.blue),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- safesync_frontend/lib/main.dart
+++ safesync_frontend/lib/main.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+import 'package:safesync_frontend/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Your App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(), // 여기서 LoginPage 위젯을 호출하여 첫 화면으로 설정
+    );
+  }
+}
+
+/*
+
 void main() {
   runApp(const MyApp());
 }
@@ -123,3 +145,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+*/
