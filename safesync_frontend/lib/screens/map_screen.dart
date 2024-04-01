1d7ab07bae6b0b7869907ba567e4b58d8d054893
CSH2315 <choish00123@gmail.com>
2024-04-01 15:11:29 +0900

    add: 하단 바 추가

--- safesync_frontend/lib/screens/map_screen.dart
+++ safesync_frontend/lib/screens/map_screen.dart
@@ -10,6 +10,7 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController mapController;
+  bool _isNavBarVisible = true;
 
   final LatLng _center = const LatLng(45.521563, -122.677433);
 
@@ -24,13 +25,74 @@ class _MapScreenState extends State<MapScreen> {
         title: const Text('Maps Sample App'),
         backgroundColor: Colors.green[700],
       ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 11.0,
-        ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 11.0,
+            ),
+          ),
+          AnimatedPositioned(
+            duration: Duration(milliseconds: 300),
+            left: 0,
+            right: 0,
+            bottom: _isNavBarVisible ? kBottomNavigationBarHeight : -kBottomNavigationBarHeight * 2,
+            child: Column(
+              children: [
+                GestureDetector(
+                  onVerticalDragUpdate: (details) {
+                    if (details.primaryDelta! < -20) {
+                      setState(() {
+                        _isNavBarVisible = false;
+                      });
+                    } else if (details.primaryDelta! > 20) {
+                      setState(() {
+                        _isNavBarVisible = true;
+                      });
+                    }
+                  },
+                  child: Container(
+                    height: kBottomNavigationBarHeight,
+                    color: Colors.white,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.map),
+                        ),
+                        // 여기에 다른 메뉴 추가
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.green[700],
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _isNavBarVisible = !_isNavBarVisible;
+                            });
+                          },
+                          icon: Icon(_isNavBarVisible ? Icons.keyboard_arrow_down : Icons.keyboard_arrow_up),
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
