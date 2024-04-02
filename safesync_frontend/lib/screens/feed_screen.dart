9aff59d44b9af615efcdff59c7cf5158f564c55e
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-04-02 16:38:13 +0900

    [backend] conflict 해결

--- safesync_frontend/lib/screens/feed_screen.dart
+++ safesync_frontend/lib/screens/feed_screen.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:safesync_frontend/providers/feed/feed_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:safesync_frontend/providers/report/report_provider.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:uuid/uuid.dart';
 
 class FeedScreen extends StatefulWidget {
   const FeedScreen({super.key});
